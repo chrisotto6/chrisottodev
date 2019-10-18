359fe76cd71401a181addf1e3aa4265f0dc663d4
Chris Otto <chris.otto6@gmail.com>
2019-10-17 22:23:57 -0500

    More more

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,31 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(results => {
+      results.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.frontmatter.slug,
+          component: path.resolve('./src/components/postLayout.js'),
+          context: {
+            slug: node.frontmatter.slug,
+          },
+        })
+      })
+      resolve()
+    })
+  })
+}
---
+++ src/components/postLayout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Layout from './layout'
+
+export default class postLayout extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>Post Layout</h1>
+      </Layout>
+    )
+  }
+}
