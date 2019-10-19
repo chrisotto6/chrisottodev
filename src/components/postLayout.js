2d9c4df1781f567e4e0b690d5b450e4524a9fb03
Chris Otto <chris.otto6@gmail.com>
2019-10-19 07:27:55 -0500

    More more more

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -4,17 +4,23 @@ import Layout from './layout'
 
 export default class postLayout extends Component {
   render() {
+    const { markdownRemark } = this.props.data
     return (
       <Layout>
-        <h1>Post Layout</h1>
+        <h1>{markdownRemark.frontmatter.title}</h1>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: markdownRemark.html,
+          }}
+        />
       </Layout>
     )
   }
 }
 
 export const query = graphql`
-  query PostQuery {
-    markdownRemark(frontmatter: { slug: { eq: "/second-post" } }) {
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
       frontmatter {
         title
