82dca6dc92d55267e64f07d4b1fc596affa60dc1
Chris Otto <chris.otto6@gmail.com>
2019-10-25 22:10:44 -0500

    One more try

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,30 +5,15 @@ const ContactPage = () => (
   <Layout>
     <h1>Contact</h1>
     <form
-      name="contact"
+      name="contact-form"
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
-      <p>
-        <label>
-          Your Name: <input type="text" placeholder="John Smith" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email:{' '}
-          <input type="email" placeholder="john@smith.com" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message" placeholder="Hello!"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+      <input name="name" placeholder="Your Name" type="text" />
+      <input name="email" placeholder="name@name.com" type="email" />
+      <textarea name="message" />
+      <button>Send</button>
     </form>
   </Layout>
 )
