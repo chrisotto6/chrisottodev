7d4b8a9bc5fcf4bac11ec85234d66b3e8c813f0a
Chris Otto <chris.otto6@gmail.com>
2019-10-26 11:42:33 -0500

    More down the rabbit hole

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,6 @@
-const percyHealthCheck = require('@percy/cypress/task')
-
-module.exports = (on, config) => {
-  on('task', percyHealthCheck)
-  return config
-}
+//const percyHealthCheck = require('@percy/cypress/task')
+//
+//module.exports = (on, config) => {
+//  on('task', percyHealthCheck)
+//  return config
+//}
