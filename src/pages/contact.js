e87a35fe91b7fb15007a895f15a31bca895c107b
Chris Otto <chris.otto6@gmail.com>
2019-10-25 21:57:50 -0500

    Adding contact form

---
+++ src/pages/contact.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Layout from '../components/common/layout'
+
+const ContactPage = () => (
+  <Layout>
+    <h1>Contact</h1>
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <p>
+        <label>
+          Your Name: <input type="text" placeholder="John Smith" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:{' '}
+          <input type="email" placeholder="john@smith.com" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message" placeholder="Hello!"></textarea>
+        </label>
+      </p>
+      <div type="hidden" data-netlify-recaptcha="true" />
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </Layout>
+)
+
+export default ContactPage
