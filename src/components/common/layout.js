b4d3b8e51afa7dabafb155bc3073ed8b31eafac5
Chris Otto <chris.otto6@gmail.com>
2019-10-29 19:40:23 -0500

    Updating tab index

--- src/components/common/layout.js
+++ src/components/common/layout.js
@@ -42,8 +42,7 @@ const Layout = ({ children }) => {
           }}
         >
           <p style={{ textAlign: `center`, verticalAlign: `bottom` }}>
-            {' '}
-            © 2014 -
+            ©
             {' '}
             {new Date().getFullYear()}
             , Built with
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,13 +12,13 @@ const ContactPage = () => (
     >
       <input type="hidden" name="form-name" value="contact" />
       <p>
-        <input name="name" aria-label="name" placeholder="John Smith" type="text" />
+        <input name="name" aria-label="name" placeholder="John Smith" type="text" tabIndex="-1" />
       </p>
       <p>
-        <input name="email" aria-label="email" placeholder="john@smith.com" type="email" />
+        <input name="email" aria-label="email" placeholder="john@smith.com" type="email" tabIndex="-2" />
       </p>
       <p>
-        <textarea name="message" aria-label="message" />
+        <textarea name="message" aria-label="message" tabIndex="-3" />
       </p>
       <p>
         <button type="button">Send</button>
