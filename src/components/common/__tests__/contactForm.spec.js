6e5fc1638856de4728ead9d25779a58d4b3e350c
Chris Otto <chris.otto6@gmail.com>
2019-11-04 20:02:16 -0600

    Part of #9, First Pass

---
+++ src/components/common/__tests__/contactForm.spec.js
@@ -0,0 +1,12 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import ContactForm from "../contactForm"
+
+describe("Contact Form", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<ContactForm />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
---
+++ src/components/common/__tests__/menu.spec.js
@@ -0,0 +1,12 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import Menu from "../menu"
+
+describe("Menu", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Menu />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
