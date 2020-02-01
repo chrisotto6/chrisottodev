8d3ca21bd6d4393920c3430c0d2a5b1d112df042
Chris Otto <chris.otto6@gmail.com>
2020-01-31 22:47:33 -0600

    Adding the portfolio page to the menu and adding a lil bit more to the hero landing

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,6 +6,7 @@ import { FaHome } from 'react-icons/fa/'
 import { FaSearch } from 'react-icons/fa/'
 import { FaEnvelope } from 'react-icons/fa/'
 import { FaTag } from 'react-icons/fa/'
+import { FaCode } from 'react-icons/fa'
 
 import Item from './Item'
 import Expand from './Expand'
@@ -18,6 +19,7 @@ class Menu extends React.Component {
     this.items = [
       { to: '/', label: 'Home', icon: FaHome },
       { to: '/tag/', label: 'Tags', icon: FaTag },
+      { to: '/project/', label: 'Projects', icon: FaCode },
       { to: '/search/', label: 'Search', icon: FaSearch },
       { to: '/contact/', label: 'Contact', icon: FaEnvelope },
     ]
