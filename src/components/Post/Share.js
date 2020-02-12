df6238752a726f432be7aeab7d6050f5f2d5ff46
Chris Otto <chris.otto6@gmail.com>
2020-02-11 21:42:42 -0600

    Updating some packages and updates components

--- src/components/Post/Share.js
+++ src/components/Post/Share.js
@@ -2,15 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {
   FacebookShareButton,
-  GooglePlusShareButton,
   LinkedinShareButton,
   TwitterShareButton,
   FacebookShareCount,
-  GooglePlusShareCount,
-  LinkedinShareCount,
   FacebookIcon,
   TwitterIcon,
-  GooglePlusIcon,
   LinkedinIcon,
 } from 'react-share'
 
@@ -45,17 +41,6 @@ const PostShare = props => {
           >
             <TwitterIcon round size={iconSize} />
           </TwitterShareButton>
-          <GooglePlusShareButton
-            url={url}
-            additionalProps={{
-              'aria-label': 'Google share',
-            }}
-          >
-            <GooglePlusIcon round size={iconSize} />
-            <GooglePlusShareCount url={url}>
-              {count => <div className="share-count">{filter(count)}</div>}
-            </GooglePlusShareCount>
-          </GooglePlusShareButton>
           <FacebookShareButton
             url={url}
             quote={`${title} - ${excerpt}`}
@@ -77,9 +62,6 @@ const PostShare = props => {
             }}
           >
             <LinkedinIcon round size={iconSize} />
-            <LinkedinShareCount url={url}>
-              {count => <div className="share-count">{filter(count)}</div>}
-            </LinkedinShareCount>
           </LinkedinShareButton>
         </div>
       </div>
@@ -96,8 +78,7 @@ const PostShare = props => {
         .links {
           display: flex;
           flex-direction: row;
-
-          :global(.SocialMediaShareButton) {
+          :global(.react-share__ShareButton) {
             margin: 0 0.8em;
             cursor: pointer;
           }
