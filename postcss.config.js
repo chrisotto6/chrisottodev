d7d1635bc497ebf0449f07475d8dbc6a724165d0
Chris Otto <chris.otto6@gmail.com>
2021-01-07 13:54:10 -0600

    Updating

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,18 @@
-module.exports = ctx => ({
-  plugins: {
-    "postcss-easy-media-query": {
+/* eslint-disable global-require */
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-nested'),
+    require('postcss-cssnext'),
+    require('postcss-text-remove-gap', {
+      defaultFontFamily: 'Open Sans',
+      defaultLineHeight: '0',
+    }),
+    require('postcss-easy-media-query', {
       breakpoints: {
         tablet: 600,
-        desktop: 1024
-      }
-    },
-    "postcss-text-remove-gap": {
-      defaultFontFamily: "Open Sans",
-      defaultLineHeight: "0"
-    },
-    "postcss-nested": {},
-    "postcss-cssnext": {}
-  }
-});
-
-// "postcss-nested": {},
-// "postcss-sorting": {
-//   order: ["custom-properties", "dollar-variables", "declarations", "at-rules", "rules"],
-//   "properties-order": "alphabetical",
-//   "unspecified-properties-position": "bottom"
-// },
-// "postcss-utilities": {},
-// "postcss-cssnext": {}
+        desktop: 1024,
+      },
+    }),
+  ],
+}
